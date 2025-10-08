6a12914ab9a44832cf9bc1ea2f21f0a9f0195ca2
ElMilaneso317 <ederjimenez317@gmail.com>
2025-10-08 09:56:24 -0600

    Actualización general del proyecto: cambios en index.html, bundle.js y recursos

--- dist/bundle.js
+++ dist/bundle.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("{\n\n//# sourceURL=webpack://unidad1/./src/index.js?\n}");
+eval("{document.addEventListener(\"DOMContentLoaded\", function () {\n  var boton = document.getElementById(\"btnVerificar\");\n  boton.addEventListener(\"click\", function () {\n    var texto = document.getElementById(\"texto\").value.trim();\n    if (texto === \"\") {\n      Swal.fire({\n        icon: \"warning\",\n        title: \"Oye viejo...\",\n        text: \"Escribe algo antes de comprobar no seas sonso\",\n        confirmButtonColor: \"#6c5ce7\"\n      });\n      return;\n    }\n    var limpio = texto.toLowerCase().replace(/\\s/g, \"\");\n    var invertido = limpio.split(\"\").reverse().join(\"\");\n    if (limpio === invertido) {\n      Swal.fire({\n        icon: \"success\",\n        title: \"¡Sí jala, es un palíndromo!\",\n        text: \"\\\"\".concat(texto, \"\\\" se lee igual al rev\\xE9s.\"),\n        confirmButtonColor: \"#0984e3\"\n      });\n    } else {\n      Swal.fire({\n        icon: \"error\",\n        title: \"No es un palíndromo (todo pndejo)\",\n        text: \"Un palíndromo es una palabra o frase que se lee igual de izquierda a derecha que de derecha a izquierda..\",\n        confirmButtonColor: \"#d63031\"\n      });\n    }\n  });\n});\n\n//# sourceURL=webpack://unidad1/./src/index.js?\n}");
 
 /***/ })
 
